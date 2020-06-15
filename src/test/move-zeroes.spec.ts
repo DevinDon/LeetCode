5a032954d0db255b92551b47157d8dfad0600b78
Devin Don <I.INF@Outlook.com>
2020-06-16 01:04:33 +0800

    feat: 移动零

---
+++ src/test/move-zeroes.spec.ts
@@ -0,0 +1,17 @@
+// import { moveZeroes } from '../main/move-zeroes';
+
+// describe('Move Zeroes\n移动零至末尾', () => {
+
+//   const tests = [
+//     { param: [[1, 0, 2]], return: 2 },
+//     { param: [[1, 0, 2, 0, 3]], return: 4 },
+//   ];
+
+//   for (const test of tests) {
+//     it(
+//       `input: ${JSON.stringify(test.param)}\toutput: ${JSON.stringify(test.return)}`,
+//       () => expect(removeDuplicates.apply(undefined, test.param as any)).toBe(test.return)
+//     );
+//   }
+
+// });
